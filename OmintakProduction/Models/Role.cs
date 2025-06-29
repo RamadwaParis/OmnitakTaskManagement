c3b24ce6bdb6203650ee03472de06e1c911714b8
RamadwaParis <ramadwaparis@gmail.com>
2025-06-29 22:28:39 +0200

    UpdateCoreBase

--- OmintakProduction/Models/Role.cs
+++ OmintakProduction/Models/Role.cs
@@ -12,5 +12,12 @@
         public int RoleId { get; set; } = 1;
         public int UserId { get; set; }
         public string RoleName { get; set; } = RoleNames.Engineer.ToString();
+
+        public string getRole(int id)
+        {
+            return ((RoleNames)id).ToString();
+        }
     }
+
+
 }
