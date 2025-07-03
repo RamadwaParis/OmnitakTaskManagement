1bdf1ee1696fc2ba3bdbd44028e1be1ba79c0548
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-07-03 15:49:53 +0200

    Add missing models and update database with migration

---
+++ OmintakProduction/Models/ProjectReport.cs
@@ -0,0 +1,11 @@
+﻿namespace OmintakProduction.Models
+{
+    public class ProjectReport
+    {
+        public int ProjectReportId { get; set; }
+        public int ProjectId { get; set; }
+        public string GeneratedBy { get; set; }
+        public DateTime ReportDate { get; set; }
+        public string Details { get; set; }
+    }
+}
