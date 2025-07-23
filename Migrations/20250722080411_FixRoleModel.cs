0172874f06d8b212fc846cc1dbf1db6e6ced9af9
Dumisani Nxumalo <dumisaninxumalo5@gmail.com>
2025-07-23 08:15:33 +0200

    Feature / Soft delete input
    _AddSoftDeleteToUser.cs/AddMissingRoles.Designer.cs/UniqueTeamUserSeed.cs/AddTeamRelationships.cs

---
+++ Migrations/20250722080411_FixRoleModel.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OmintakProduction.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRoleModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Role");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Role",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: 1,
+                column: "UserId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: 2,
+                column: "UserId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: 3,
+                column: "UserId",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: 4,
+                column: "UserId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: 5,
+                column: "UserId",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: 6,
+                column: "UserId",
+                value: 6);
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: 7,
+                column: "UserId",
+                value: 7);
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "RoleId",
+                keyValue: 8,
+                column: "UserId",
+                value: 8);
+        }
+    }
+}
